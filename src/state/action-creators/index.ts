0eb5236f9a345f3c7f22456af1c42d4df980a00e
Francisco <javiermartincuenca@gmail.com>
2021-02-09 18:46:51 +0100

    Setup reducer for cells

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,43 @@
+import { ActionType } from '../action-types';
+import {
+  DeleteCellAction,
+  InsertCellBeforeAction,
+  MoveCellAction,
+  UpdateCellAction,
+} from '../actions';
+import { CellMovesTypes, CellTypes } from '../cell';
+
+export const updateCell = (id: string, content: string): UpdateCellAction => ({
+  type: ActionType.UPDATE_CELL,
+  payload: {
+    id,
+    content,
+  },
+});
+
+export const deleteCell = (id: string): DeleteCellAction => ({
+  type: ActionType.DELETE_CELL,
+  payload: id,
+});
+
+export const moveCell = (
+  id: string,
+  direction: CellMovesTypes
+): MoveCellAction => ({
+  type: ActionType.MOVE_CELL,
+  payload: {
+    id,
+    direction,
+  },
+});
+
+export const insertCellBefore = (
+  id: string,
+  cellType: CellTypes
+): InsertCellBeforeAction => ({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id,
+    type: cellType,
+  },
+});
