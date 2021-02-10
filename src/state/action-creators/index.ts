3334a63d70d7f956420b7bec898a136d2c2a6ceb
Francisco <javiermartincuenca@gmail.com>
2021-02-10 14:33:26 +0100

    Add cell

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -32,7 +32,7 @@ export const moveCell = (
 });
 
 export const insertCellBefore = (
-  id: string,
+  id: string | null,
   cellType: CellTypes
 ): InsertCellBeforeAction => ({
   type: ActionType.INSERT_CELL_BEFORE,
