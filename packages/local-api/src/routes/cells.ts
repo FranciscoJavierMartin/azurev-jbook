81986c23b9ba6bd7ddf20d0dea671d16a566ece6
Francisco <javiermartincuenca@gmail.com>
2021-02-12 14:41:31 +0100

    Read and write notebooks

---
+++ packages/local-api/src/routes/cells.ts
@@ -0,0 +1,37 @@
+import { Router, Request, Response, json } from 'express';
+import fs from 'fs/promises';
+import path from 'path';
+
+interface Cell {
+  id: string;
+  content: string;
+  type: 'text' | 'code';
+}
+
+export const createCellsRouter = (filename: string, dir: string): Router => {
+  const router = Router();
+  router.use(json())
+  const fullPath = path.join(dir, filename);
+
+  router.get('/cells', async (req: Request, res: Response) => {
+    try {
+      const result = await fs.readFile(fullPath, { encoding: 'utf-8' });
+      res.send(JSON.parse(result));
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        await fs.writeFile(fullPath, '[]', 'utf-8');
+        res.send([]);
+      } else {
+        throw err;
+      }
+    }
+  });
+
+  router.post('/cells', async (req: Request, res: Response) => {
+    const { cells }: { cells: Cell[] } = req.body;
+    await fs.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
+    res.send({ status: 'ok' });
+  });
+
+  return router;
+};
